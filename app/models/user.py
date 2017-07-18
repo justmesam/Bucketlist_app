7ff4c40669cff681fff30f17de225e4421d1ecf1
justmesam <sthiongo12@gmail.com>
2017-07-19 02:32:16 +0300

    [FEATURE] Add login verify and current user methods

--- app/models/user.py
+++ app/models/user.py
@@ -1,5 +1,6 @@
 """ the user module """
 import uuid
+from werkzeug.security import check_password_hash
 from app.models.data import Data
 from app.models.bucketlist import Bucketlist
 
@@ -11,13 +12,29 @@ class User(object):
         self.username = username
         self.email = email
         self.password = password
-        self._id = uuid.uuid4().hex
+        self._id = uuid.uuid4().hex if _id is None else _id
 
     @staticmethod
     def user_exists(email):
         """method checks if the user already exist"""
         data = [i['email'] for i in Data.users if email == i['email']]
         return "".join(data) == email
+
+    @staticmethod
+    def user_login_verify(email, password):
+        """ methods verifys user password and email"""
+        user_exist = User.user_exists(email)
+        if user_exist is True:
+            emails_password = "".join([i['password']\
+             for i in Data.users if email == i['email']])
+            return emails_password == password
+        return False
+
+    @staticmethod
+    def get_username(email):
+        """Gets users username for use in session at login"""
+        username = [i['username'] for i in Data.users if email == i['email']]
+        return "".join(username)
 
     @classmethod
     def register(cls, name, username, email, password):
@@ -49,13 +66,33 @@ class User(object):
         bucketlist_.save_to_bucketlists()
 
     @staticmethod
-    def create_item(bucketlist_id, item_name):
+    def create_item(bucketlist_id, item_name, description):
         """method used to create bucketlist items"""
         _id = bucketlist_id
         data = Data.get_the_dictionary(_id, Data.bucketlists)
-        bucketlist = Bucketlist(data['title'], data['owner'], data['intro'], data['owner_id'])
-        bucketlist.new_item(item_name=item_name)
+        bucketlist = Bucketlist(data['title'],
+                                data['owner'],
+                                data['intro'],
+                                data['owner_id'],
+                                data['_id'])
+        bucketlist.new_item(item_name=item_name,
+                            description=description)
 
     def save_to_users(self):
         """this method saves the user to users"""
         Data.add_data(self.user_data())
+
+    @staticmethod
+    def current_user(username):
+        """
+        method gets user details using the session username to create the instance
+         of the user logged so as to create a bucketlist"""
+        user = [i for i in Data.users if username == i['username']]
+        user = user[0]
+        return (
+            user['name'],
+            user['username'],
+            user['email'],
+            user['password'],
+            user['_id']
+            )
