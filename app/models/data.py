8102210ff0fcb60a39c9c17fb54ccc4e51a84469
justmesam <sthiongo12@gmail.com>
2017-07-19 02:34:33 +0300

    [FEATURE] Add method for deleting a dictionary and method for getting index of a dictionary

--- app/models/data.py
+++ app/models/data.py
@@ -28,3 +28,16 @@ class Data(object):
             return data_
         else:
             return "id don't exist"
+
+    @staticmethod
+    def get_index(_id, arg):
+        """gets the index of a dictionary in a list"""
+        index_data = [i for i in arg if _id == i['_id']]
+        _index = arg.index(index_data[0])
+        return _index
+
+    @staticmethod
+    def delete_dictionary(_id, arg):
+        """ deletes the dictionary in a list"""
+        index_ = Data.get_index(_id, arg)
+        del arg[index_]
