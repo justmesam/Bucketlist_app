d2b0c8d3a40a40575755df1ac0871b5edfdf094e
justmesam <sthiongo12@gmail.com>
2017-07-19 14:18:55 +0300

    Add CSRF secret key

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 """ contains all flask configurations"""
 DEBUG = True
 SECRET_KEY = "This_is_a_very_complicated_secret_key_==>_x0eHx9ex855xc8x1c95"
+WTF_CSRF_SECRET_KEY = "This_is_a_very_complicated_secret_key_==>_x0eHx9ex855xc8x1c95"
