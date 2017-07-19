f764accbaa9d78f1ebe45fccc21b6f9821a0e980
justmesam <sthiongo12@gmail.com>
2017-07-19 13:30:16 +0300

    [CHORE] Add tests for login and logout

--- tests/test_flask.py
+++ tests/test_flask.py
@@ -1,4 +1,4 @@
-'''""" flask testing module """
+""" flask testing module """
 
 from unittest import TestCase
 from app import app
@@ -10,21 +10,57 @@ class TestClass(TestCase):
         app.config['TESTING'] = True
         self.app = app.test_client()
 
-    def test_index_page(self):
+#### ===>>  HELPER METHODS <=== ####
+
+    def login(self, email, password):
+        """ login helper """
+        return self.app.post('/login',
+                             data=dict(email=email,
+                                       password=password),
+                             follow_redirects=True)
+    def register(self, name, username, email, password):
+        """register helper """
+        return self.app.post('/register',
+                             data=dict(name=name,
+                                       email=email,
+                                       username=username,
+                                       password=password),
+                             follow_redirects=True)
+    def logout(self):
+        """logout helper   """
+        return self.app.get('/logout',
+                            follow_redirects=True)
+
+    def test_misc_pages(self):
+        """tests the response of the miscellaneous pages"""
+        ## >> index page << ##
         result = self.app.get('/', follow_redirects=True)
         result1 = self.app.get('/index', follow_redirects=True)
         self.assertEqual(result.status_code, 200)
         self.assertEqual(result1.status_code, 200)
 
-
-    def test_about_page(self):
+        ## >> about page << ##
         result = self.app.get('/about', follow_redirects=True)
         self.assertEqual(result.status_code, 200)
 
-    def test_FAQ_page(self):
+        ## >> faqs page << ##
         result = self.app.get('/faqs', follow_redirects=True)
         self.assertEqual(result.status_code, 200)
 
+    def test_login_success(self):
+        result = self.login('admin@admin.local', 'default')
+        self.assertEqual(result.status_code, 200)
+        self.assertIn(b'Bucketlist', result.data)
+
+    def test_login_invalid(self):
+         result = self.login('admin', 'defaultx')
+         self.assertEqual(result.status_code, 200)
+         self.assertIn(b'Home', result.data)
+
+
+    def test_logout(self):
+        result = self.logout()
+        self.assertTrue(b'login' in result.data)
+
 if __name__ == '__main__':
     unittest.main()
-'''
